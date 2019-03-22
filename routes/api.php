24c3ed1b2a3984f735cc58acfa6f05dda0275243
SujithJr <sujithsrt10@gmail.com>
2019-03-23 01:08:06 +0530

    Added Passport Authentication

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,20 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['json.response']], function () {
+
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // public routes
+    Route::post('/login', 'Api\AuthController@login')->name('login');
+    Route::post('/register', 'Api\AuthController@register')->name('register');
+
+    // private routes
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/logout', 'Api\AuthController@logout')->name('logout');
+        Route::get('users/me', 'HomeController@index');
+    });
+
 });
